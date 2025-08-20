82d41e81cba6660e64959bd3a4377616480438a7
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-20 09:49:07 +0600

    Dom Event handler practice done

--- Dom Events/js/handler.js
+++ Dom Events/js/handler.js
@@ -2,3 +2,9 @@ document.getElementById('btn-update-title').addEventListener('click', function (
     const pageTitleElement = document.getElementById('page-title');
     pageTitleElement.innerText = "Update page title text"
 })
+
+const btnLogin = document.getElementById('btn-login');
+btnLogin.addEventListener('click', function () {
+    const userInfo = document.getElementById('user-info');
+    userInfo.innerText = 'User login successfully';
+})
